5d92aae3e1133f111e40bcabb161b0be0f3e8dc4
Yuje Lee <dbwp031@gmail.com>
2023-05-14 17:49:02 +0900

    first commit

---
+++ src/main/java/com/dylc/DYLC/DylcApplication.java
@@ -0,0 +1,13 @@
+package com.dylc.DYLC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DylcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DylcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dylc/DYLC/DylcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dylc.DYLC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DylcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
